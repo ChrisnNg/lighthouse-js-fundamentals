9d6dd30ef43ad38def3c26da385ac5c077b9c977
ChrisnNg <cristopherng@hotmail.com>
2019-07-24 00:05:51 +0000

    added array filter, function, object, and object properties js

---
+++ objects.js
@@ -0,0 +1,14 @@
+var pizza = {  
+  toppings: ['cheese', 'sauce', 'pepperoni'],  
+  crust: 'deep dish',  
+  serves: 2  
+};  
+/*
+Use console.log() to print the pizza object to the terminal.  
+
+Check to see if your program is correct by running this command:  
+
+javascripting verify objects.js  
+*/
+
+console.log(pizza);
